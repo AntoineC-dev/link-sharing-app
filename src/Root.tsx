defceb2f3439a6fe279955495eaaa1f6df7b8757
Antoine Cheminat <ac.dev@hotmail.com>
2023-10-15 08:57:46 -0400

    Setup basic toast notifications

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { Outlet } from 'react-router-dom';
 import { firebaseAuth } from './firebase';
 import { useStore } from './stores';
+import { Toaster } from 'react-hot-toast';
 
 function Root() {
   const setUser = useStore((store) => store.setUser);
@@ -16,7 +17,22 @@ function Root() {
     });
     return () => listen();
   }, [setUser]);
-  return <Outlet />;
+  return (
+    <>
+      <Outlet />
+      <Toaster
+        position="bottom-center"
+        toastOptions={{
+          style: {
+            color: 'hsl(var(--clr-neutral-200))',
+            fontSize: 'var(--fs-base)',
+            fontWeight: 'var(--fw-semibold)',
+            backgroundColor: 'hsl(var(--clr-neutral-500))',
+          },
+        }}
+      />
+    </>
+  );
 }
 
 export default Root;
--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,12 +1,11 @@
 import { User } from 'firebase/auth';
 import { create } from 'zustand';
 import { firebaseAuth } from '../firebase';
+import toast from 'react-hot-toast';
 
 type Store = {
   user: User | null;
-  error: string | null;
   setUser: (user: User | null) => void;
-  setError: (error: string | null) => void;
   login: (email: string, password: string) => void;
   createAccount: (email: string, password: string) => void;
   logout: () => void;
@@ -14,16 +13,14 @@ type Store = {
 
 const useStore = create<Store>()((set) => ({
   user: null,
-  error: null,
   setUser: (user) => set((state) => ({ ...state, user })),
-  setError: (error) => set((state) => ({ ...state, error })),
   login: async (email, password) => {
     const error = await firebaseAuth.login(email, password);
-    if (error) set((state) => ({ ...state, error }));
+    if (error) toast.error(error);
   },
   createAccount: async (email, password) => {
     const error = await firebaseAuth.createAccount(email, password);
-    if (error) set((state) => ({ ...state, error }));
+    if (error) toast.error(error);
   },
   logout: () => firebaseAuth.logout(),
 }));
