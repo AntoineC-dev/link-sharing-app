58864c299c8b60ffad2ce8ff5a6e4b6434083949
Antoine Cheminat <ac.dev@hotmail.com>
2023-10-16 06:16:50 -0400

    Update Navbar & Add logout button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,19 @@
+import { Link, NavLink } from 'react-router-dom';
 import styles from './Navbar.module.css';
+import { useStore } from '../../stores';
 import LogoSmall from '../../assets/logo-devlinks-small.svg?react';
+import LogoLarge from '../../assets/logo-devlinks-large.svg?react';
 import IconLinks from '../../assets/icon-links-header.svg?react';
 import IconPreview from '../../assets/icon-preview-header.svg?react';
 import IconProfile from '../../assets/icon-profile-details-header.svg?react';
-import { Link, NavLink } from 'react-router-dom';
+import IconLogout from '../../assets/icon-signout.svg?react';
 
 function Navbar() {
+  const logout = useStore((store) => store.logout);
   return (
     <header className={styles.container}>
-      <LogoSmall className={styles.logo} />
+      <LogoSmall className={styles.logo} data-screen="small" />
+      <LogoLarge className={styles.logo} data-screen="large" />
       <nav className={styles.nav}>
         <NavLink to={'/app'} className={styles.navLink} aria-label="Links" end>
           <IconLinks />
@@ -19,10 +24,15 @@ function Navbar() {
           <span>Profile Details</span>
         </NavLink>
       </nav>
-      <Link to={'/preview'} className={styles.link} aria-label="Preview">
-        <IconPreview />
-        <span>Preview</span>
-      </Link>
+      <div className={styles.actions}>
+        <Link to={'/preview'} className={styles.link} aria-label="Preview">
+          <IconPreview />
+          <span>Preview</span>
+        </Link>
+        <button onClick={logout} className={styles.logout} aria-label="Logout">
+          <IconLogout />
+        </button>
+      </div>
     </header>
   );
 }
